dd0976879cf65c2656365c3a44747ed281594796
SISULU255 <97722123+SISULU255@users.noreply.github.com>
2022-09-28 15:55:00 +0300

    Update BOT-QN-ANS.py

--- BOT-QN-ANS.py
+++ BOT-QN-ANS.py
@@ -7,8 +7,20 @@ context = """KIITEC is a technical institution registered by NACTE (REG/EOS/027)
 
 Resting on a 15-acre campus, KIITEC offers the most advanced targeted training technologies in the region. The innovative education model developed at KIITEC is based on a hands-on and student-centered approach to learning with full access to modern learning equipment simulating real world practical experiences. The training center is registered and accredited by the National Council for Technical Education (NACTE) and awards successful graduates with a 3-year National Technical Award Level Six (NTA-6) Diploma.
 KIITEC’s specialized training programs include: Electronics & Telecommunication Engineering, Industrial Automation, Computer Engineering and Networking , Renewable Energies, Environmental Impact
+Future training programs in development:  Biomedical , Avionics.
+ELECTRICAL AND INDUSTRIAL AUTOMATION ENGINEERINg this  programme focuses on combined skills that are in Electrical and Industrial Automation engineering. it has been noted that Tanzania has about 81% of population (43,621,499) own and uses mobile phone for their daily activities in 2018 compared to 61% (27,607,822) in 2013. Also number of electricity connection in Tanzania mainland has increased to 73% in 2018 compared to 13% in 2007. Furthermore, the country has been constructing stiegler-gorge hydropower plant that is expected to be finalized in 42 months and should have the capacity to produce 2,115 MW by April 2022, dramatically increasing the country’s current power production capacity of 1,553.96 MW to 3668.96MW. Therefore, improvement airports, construction of standard gauge railways, establishment of TV and Radio stations, shall increase of industries and extension of National grid to rural community will increase the demand of technician in this cadre. The programme will admit students with ordinary level of education with any two passes in science subjects.
+requirements of ELECTRICAL AND INDUSTRIAL AUTOMATION ENGINEERINg is Possession of a Certificate of Secondary Education Examinations (CSEE) with a minimum of FOUR passes of which TWO must be among the following subjects: Mathematics, Physics / Engineering science, Biology and Chemistry; excluding religious subjects
+or
+Possession of a National Vocational Award (NVA) Level III in Electrical, Electronics, Telecommunication, Mechanical and related field offered by VETA and a Certificate of Secondary Education Examination (CSEE) with at least two passes.
+Diploma in Telecommunication Engineering,this course reflects the role of a technician with a range of telecommunications skills who can install and maintain enterprise network in emerging and converging technologies, optical and wireless equipment for high speed broadband network infrastructure, internet protocol (IP), MPLS based network telecommunications equipment, IP based networks in home networks and small and medium enterprises, telecommunications access network cabling and infrastructure, systems and basic customer premises equipment, assess installation requirements of converging voice, video and data IP networks, plan and performing installations, test installed equipment and fault find.
+requirements of Diploma in Telecommunication Engineering is Possession of a Certificate of Secondary Education Examinations (CSEE) with a minimum of FOUR passes of which TWO must be among the following subjects: Mathematics, Physics / Engineering science, Biology and Chemistry; excluding religious subjects
+My name is SISULU and my creator is WALTER RICHARD.
 
-Future training programs in development:  Biomedical , Avionics"""
+
+
+
+
+"""
 
 st.write(qa_model(question = question, context = context))
 ## {'answer': 'İstanbul', 'end': 39, 'score': 0.953, 'start': 31}
