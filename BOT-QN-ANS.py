8a8b165a832419c3fe30d472be01629477f3639b
SISULU255 <97722123+SISULU255@users.noreply.github.com>
2022-09-30 15:17:40 +0300

    Update BOT-QN-ANS.py

--- BOT-QN-ANS.py
+++ BOT-QN-ANS.py
@@ -27,7 +27,7 @@ Fee structure and Mode of Payment for Diploma Programmes,for first semister is 	
 """
 ,
   'question':st.text_input('Question', 'what is kiitec')
-  if question =="":
+  if 'question' == "" :
     st.write("Write the question in short sentence so that i could help !")
   
 })
